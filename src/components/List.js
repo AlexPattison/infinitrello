5ac72f838dcae5c998092548cca475ee68f4efb2
Alex Pattison <alexlpattison@gmail.com>
2017-10-07 20:13:54 -0700

    Implement a minimal List component
    Cards will be nested under Lists and Lists will be nested under a Board

---
+++ src/components/List.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Card from './Card';
+
+const List = ({ title, body, children, state }) => {
+  const Cards = children.map((nodeId) => {
+    const { title, body } = state.nodes[nodeId];
+
+    return <Card title={title} body={body} />
+  });
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <ul>{Cards}</ul>
+    </div>
+  );
+};
+
+export default List;
