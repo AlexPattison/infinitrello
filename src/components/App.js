a1e4d578f8ff9929ad55fb97b0266371f33e7ebf
Alex Pattison <alexlpattison@gmail.com>
2017-10-07 21:07:38 -0700

    Pass seed data to Board

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import Board from './Board';
+import state from '../assets/seed-data';
+
+const { title, body, children } = state.nodes[1];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         Infinitrello
+        <Board title={title} body={body} children={children} state={state}/>
       </div>
     );
   }
